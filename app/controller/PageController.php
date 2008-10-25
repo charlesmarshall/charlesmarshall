084177e7d40851e2f4e29d9c77ffbfcab4ba59c2
charles <charles@oneblackbear.com>
2008-10-25 02:03:18 +0100

    loads of stuff

--- app/controller/PageController.php
+++ app/controller/PageController.php
@@ -3,7 +3,7 @@
 class PageController extends ApplicationController {
 
 	public $this_page = 1;
-	public $per_page = 4;
+	public $per_page = 10;
 	
 	public function controller_global(){
 		parent::cms();
@@ -16,6 +16,27 @@ class PageController extends ApplicationController {
 		$this->cms_content[0] = $model->filter(array('cms_section_id'=>3))->order('published DESC')->first();
   }
   
+
+	public function related(){
+		$this->use_layout = false;
+		$this->use_view ="_related";
+		if(Request::post('page_number') && Request::post('section') ){
+			$page = parse_url(Request::post('page_number'));
+			$page = str_replace("page=","",$page['query']);
+			if($page && Request::post('section')){
+				$this->this_page = $page;
+				$section = new CmsSection;
+				$content = new CmsContent("published");
+				$this->cms_section = $section->filter(array('url'=>Request::post('section') ))->first();
+				if($this->cms_section->id){
+					$this->cms_section_id = $this->cms_section->id;
+					$this->cms_content = $content->filter(array('cms_section_id'=>$this->cms_section_id))->order('published DESC')->page($this->this_page,$this->per_page);
+				}
+			}
+		}
+		
+	}
+
 }
 
 ?>
--- app/lib/SiteHelper.php
+++ app/lib/SiteHelper.php
@@ -2,7 +2,7 @@
 class SiteHelper extends WXHelpers {
   	
 	public function short_date($date){
-		return date("dS M", strtotime($date));
+		return date("dS M y", strtotime($date));
 	}
 	public function long_date($date){
 		return date("nS F Y", strtotime($date));
