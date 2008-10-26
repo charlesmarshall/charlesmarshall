734045ef12aeb401605f9e9ccdc57003338cda07
charles <charles@oneblackbear.com>
2008-10-26 01:28:22 +0100

    ie

--- app/controller/ApplicationController.php
+++ app/controller/ApplicationController.php
@@ -10,9 +10,15 @@ class ApplicationController extends CMSApplicationController{
 	public $flickr_user = "81461873@N00";
 	public $flickr_secret_key = "c376479878c604c3a4a0b9af31930313";
 	public $server = false;
+	public $warned = false;
 	
 	public function __construct(){
 		$this->server ="http://".$_SERVER['HTTP_HOST']."/";
+		
+		if(Request::param('warned') || Session::get('warned')){
+			Session::set('warned', 1);
+			$this->warned = true;
+		}
 	}
 	
 	public function get_body_id(){
