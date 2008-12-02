080db7fd92612bff8eeb16ffee4da05a82514f63
charles <charles@oneblackbear.com>
2008-12-02 23:41:27 +0000

    diagonal slider! :-o

--- public/diagonal-slider/jquery.diagonal-slider.js
+++ public/diagonal-slider/jquery.diagonal-slider.js
@@ -10,7 +10,7 @@
 			});
 			jQ(this).find(params['contents']).each(function(){
 				$.diagonalslider.addtargets(this,params['angle'], params['targetsize'], params['triggers']);
-				$.diagonalslider.hoveraction(this, params['items'], params['angle'],params['overlays'], params['closer'], params['targetsize']);
+				$.diagonalslider.hoveraction(this, params['items'], params['angle'],params['overlays'], params['closer'], params['targetsize'],params['speed']);
 			});
 			
 		});
@@ -26,19 +26,17 @@
 				jQ(target).prepend("<div class='hover_status' style='position:absolute;top:"+((i*size)+offseth)+"px;left:"+(((i*size)+offsetx)+size)+"px;height:"+size+"px;width:"+(size)+"px;'>&nbsp;</div>"); //main block
 			}
 		},
-		hoveraction:function(target, items, angle,overlays, closer, size){
+		hoveraction:function(target, items, angle,overlays, closer, size, speed){
 			var moveable = overlays['moving'], w=(jQ(target).outerWidth()*0.75),multi=(angle/90);
 			//setup the hover motion, so on hover it expands
 			jQ(target).find('.hover_target').hover(
 				function(){
 					jQ(moveable).each(function(){						
-						jQ(this).animate({'left':(w+this._dsoleft)+'px'});
+						jQ(this).animate({'left':(w+this._dsoleft)+'px'}, speed);
 						jQ(target).find(items).css('padding-left', (w/3)+'px');
-						var item_counter=0;
-						jQ(target).find(items).find('*').each(function(){
+						jQ(target).find(items).children('p,h1,h2,h3,h4,h5,h6,li').each(function(){
 							var t= jQ(this).eq(0).offset().top;
 							jQ(this).css('padding-left', (t)+'px');
-							item_counter++;
 						});
 					});
 				}, 
@@ -47,19 +45,23 @@
 			//do nothing on individuals
 			jQ(items).hover(function(){}, function(){});	
 			//on hover close it
-			jQ(closer).hover(
-				function(){}, 
+			jQ(closer).hover(				 
 				function(){
 					jQ(moveable).each(function(){
-						jQ(this).animate({'left':(this._dsoleft)+'px'});	
-						jQ(target).find(items).css('padding-left', '0px');					
+						jQ(this).animate({'left':(this._dsoleft)+'px'}, speed);	
+						jQ(target).find(items).css('padding-left', '0px');
+						jQ(target).find(items).children('p,h1,h2,h3,h4,h5,h6,li').each(function(){
+							var t= jQ(this).eq(0).offset().top;
+							jQ(this).css('padding-left', '0px');
+						});					
 					});
-				}
+				},
+				function(){}
 			);
 		}
 	};
 	
-	$.fn.diagonalslider.defaults = {start:1,items:'.item', triggers: '#triggers', closer:'body',contents:'#contents', angle:45,speed: 'slow', targetsize:100, overlays:{fixed:'#diagonal_overlay_1', moving:'#diagonal_overlay_2'} };
+	$.fn.diagonalslider.defaults = {start:1,items:'.item', triggers: '#triggers', closer:'body',contents:'#contents', angle:45,speed: 'fast', targetsize:100, overlays:{fixed:'#diagonal_overlay_1', moving:'#diagonal_overlay_2'} };
 	var jQ = jQuery, dsoleft=0,params, elecount=0,H=$.diagonalslider.hash,	ie6=$.browser.msie&&($.browser.version == "6.0");;
 	
 })(jQuery);
