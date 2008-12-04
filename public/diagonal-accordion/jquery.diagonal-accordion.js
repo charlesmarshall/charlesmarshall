2cb03c3f8fab2b242738340a207acb36b9be135c
charles <charles@oneblackbear.com>
2008-12-04 10:11:24 +0000

    making the diagonal beast work for ie

--- public/diagonal-accordion/jquery.diagonal-accordion.js
+++ public/diagonal-accordion/jquery.diagonal-accordion.js
@@ -1,4 +1,5 @@
 (function($) {
+	/* your typical accordion, but at 45 degrees!*/
 	$.fn.diagonalaccordion = function(options){		
 		params = $.extend({}, $.fn.diagonalaccordion.defaults, options);
 		return this.each(function(){
@@ -26,7 +27,7 @@
 					function(){}
 				);
 			});
-			
+			if(params['start']) $.diagonalaccordion.slide(elecount, params['start']);
 		});
 	}
 	
@@ -42,11 +43,10 @@
 			var h = H[elenum], params=h.params, container=h.container,barnum = bar._digbar, zindex=(1000-barnum), size=params.bar_size;
 			var offsetx = jQ(container).eq(0).offset().left+(params.bar_size*(barnum-1));
 			var offsety = h.toppos;
-			var numtargets = Math.round(h.cheight/size);
+			var numtargets = Math.ceil(h.cheight/size);
 			for(i=0;i<numtargets;i++){
 				jQ(container).prepend("<div class='hover_target target_"+barnum+"' rel='"+barnum+"' style='z-index:"+zindex+";position:absolute;top:"+((i*size/2)+offsety)+"px;left:"+((i*(size/2))+offsetx+(size/2))+"px;height:"+(size/2)+"px;width:"+(size/2)+"px;'>&nbsp;</div>"); //main block
 			}
-			jQ(container).find('.target');			
 		},
 		onhover:function(elenum, target){
 			var h = H[elenum], params=h.params, container=h.container, accbar = jQ(target).attr('rel'), accordion = h.items[accbar];
@@ -59,29 +59,30 @@
 			if(!h.open){	//not open, so open it
 				for(i=newbar+1; i<=ac_count;i++) jQ(container).find('.target_'+i+', .bar_'+i).animate({left:'+='+awidth+'px'},h.speed);
 			}else if(newbar > h.open){
-				for(i=newbar; i>h.open;i--) jQ(container).find('.target_'+i+', .bar_'+i).animate({left:'-='+awidth+'px'},h.speed);
+				for(i=newbar; i>h.open;i--) jQ(container).find('.target_'+i.toString()+', .bar_'+i).animate({left:'-='+awidth+'px'},h.speed);
 			}else if(newbar < h.open){
 				for(i=h.open; i>newbar;i--) jQ(container).find('.target_'+i+', .bar_'+i).animate({left:'+='+awidth+'px'},h.speed);
 			}
 			//padding to make text staggered
 			jQ(container).children(params.accordion).css('display', 'none');
-			//switch zindexes
+			//switch zindexes			
 			jQ(container).find('.target_'+h.open).each(function(){
-				jQ(this).css('z-index', jQ(this).css('z-index').replace('-',''));
+				jQ(this).css('z-index', jQ(this).css('z-index').toString().replace('-',''));
 			});
 			jQ(container).find('.target_'+newbar+', .acc_bar').each(function(){
-				jQ(this).css('z-index', '-'+jQ(this).css('z-index'));
-			});
-			jQ(container).children('.accordion_item_'+newbar).css('display', 'block').children('p,h1,h2,h3,h4,h5,h6,li').each(function(){
-				var t= jQ(this).eq(0).offset().top;
-				jQ(this).css('padding-left', (t+(newbar*params.bar_size)+(params.bar_size*2))+'px').css('z-index',200);
+				var nz = parseInt(jQ(this).css('z-index'));
+				jQ(this).css('z-index', nz-(nz*2));
 			});
 			
-			h.open = accbar;			
+			jQ(container).children('.accordion_item_'+newbar).css('display', 'block').children('p,h1,h2,h3,h4,h5,h6,li').each(function(){
+				var t= jQ(this).eq(0).offset().top;
+				jQ(this).css('padding-left', (t+(newbar*params.bar_size)+(params.bar_size*2))+'px').css('padding-right', params.bar_size+'px').css('z-index',200);
+			});			
+			h.open = newbar;			
 		}
 	};
 	
-	$.fn.diagonalaccordion.defaults = {acc_width:500, acc_height:300,bar_size:45, speed:'slow', accordion:'.accordion'};
+	$.fn.diagonalaccordion.defaults = {start:false,acc_width:500, acc_height:300,bar_size:45, speed:'slow', accordion:'.accordion'};
 	var jQ = jQuery, params, elecount=0,H=$.diagonalaccordion.hash,	ie6=$.browser.msie&&($.browser.version == "6.0");;
 	
 })(jQuery);
