1da3d016b69d15daf66045e674995f6cb2e07963
charles <charles@oneblackbear.com>
2009-03-26 17:07:15 +0000

    rename

--- public/smartmodal/jquery.smartmodal.js
+++ public/smartmodal/jquery.smartmodal.js
@@ -1,101 +1,109 @@
+/**
+modal_content => sm_content
+modal_overlay => sm_olay
+modal_close => sm_close
+modal_title => sm_title
+fn.modal* => fn.smart_modal*
+modal_count => sm_count
+**/
 (function($) {
 	//base function to call and setup everything
-	$.fn.modal=function(options){
+	$.fn.smart_modal=function(options){
 		return this.each(function(){			
-			if(this._modal) return; //if already a modal return
-			if(typeof(options) != "undefined")	var params = $.extend({}, $.fn.modal.defaults, options); //if some options are passed in merge them
-			else var params = $.fn.modal.defaults;
-			if(typeof(modal_count) == "undefined") modal_count=0; //set the counter to 0
-			modal_count++;
-			this._modal=modal_count; //set what modal number this is
-			H[modal_count] = {config:params,target_modal:this}; //add to hash var
-			$(this).modal_add_show(this); //add show & hide triggers
+			if(this._sm) return; //if already a modal return
+			if(typeof(options) != "undefined")	var params = $.extend({}, $.fn.smart_modal.defaults, options); //if some options are passed in merge them
+			else var params = $.fn.smart_modal.defaults;
+			if(typeof(sm_count) == "undefined") sm_count=0; //set the counter to 0
+			sm_count++;
+			this._sm=sm_count; //set what modal number this is
+			H[sm_count] = {config:params,target_modal:this}; //add to hash var
+			$(this).smart_modal_add_show(this); //add show & hide triggers
 		});
 	};
-	$.fn.modal_add_show=function(ele){ return $.modal.show(ele); };
+	$.fn.smart_modal_add_show=function(ele){ return $.smart_modal.show(ele); };
 	//extra function so show & hide can be called
-	$.fn.modal_show=function(){
+	$.fn.smart_modal_show=function(){
 		return this.each(function(){
-			$.modal.open(this);
+			$.smart_modal.open(this);
 		});		
 	};
-	$.fn.modal_hide=function(){
+	$.fn.smart_modal_hide=function(){
 		return this.each(function(){
-			$.modal.hide(this, true);
+			$.smart_modal.hide(this, true);
 		});		
 	};
 	//the default config vars
-	$.fn.modal.defaults = {show:false, hide:false, modal_styles:{display:"block", zIndex:1001}, resize:true, hide_on_overlay_click:true };
+	$.fn.smart_modal.defaults = {show:false, hide:false, modal_styles:{display:"block", zIndex:1001}, resize:true, hide_on_overlay_click:true };
 	//the over riden stuff
-	$.modal = {
+	$.smart_modal = {
 		hash:{}, //the hash used to store all the configs & targets
 		show:function(ele){
-			var pos = ele._modal, h = H[pos];
+			var pos = ele._sm, h = H[pos];
 			jQ(h.target_modal).click(function(){
-				$.modal.open(ele);
+				$.smart_modal.open(ele);
 				return false;
 			});
 			return false;
 		},
 		
 		hide:function(ele, force){
-			var pos = ele._modal, h = H[pos];			
-			if(h.config.hide_on_overlay_click) var idstr = "#modal_overlay, .modal_close";
-			else var idstr = ".modal_close";
-			if(force) $.modal.remove(ele);
+			var pos = ele._sm, h = H[pos];			
+			if(h.config.hide_on_overlay_click) var idstr = "#sm_olay, .sm_close";
+			else var idstr = ".sm_close";
+			if(force) $.smart_modal.remove(ele);
 			jQ(idstr).click(function(){
-       	$.modal.remove(ele);
+       	$.smart_modal.remove(ele);
 				return false;
       });
 		},
 		remove:function(ele){
-			var pos = ele._modal, h = H[pos];
-			jQ("#modal_content").remove();
-			jQ("#modal_overlay").remove();							
+			var pos = ele._sm, h = H[pos];
+			jQ("#sm_content").remove();
+			jQ("#sm_olay").remove();							
 			if(h.config.hide)	h.config.hide();
 		},
 		open:function(ele){
-			var pos = ele._modal;
+			var pos = ele._sm;
 			var h = H[pos];			
-			$.modal.insert_overlay();
-			$.modal.insert_content_container();
-			var content = $.modal.get_content($(h.target_modal));
-			jQ("#modal_content").html(content);			
-			if(h.config.modal_styles) jQ("#modal_content").css(h.config.modal_styles);
-			if(h.config.resize) $.modal.resize_container();
-      $.modal.for_ie(jQ("#modal_overlay"));	
+			$.smart_modal.insert_overlay();
+			$.smart_modal.insert_content_container();
+			var content = $.smart_modal.get_content($(h.target_modal));
+			jQ("#sm_content").html(content);			
+			if(h.config.modal_styles) jQ("#sm_content").css(h.config.modal_styles);
+			if(h.config.resize) $.smart_modal.resize_container();
+      $.smart_modal.for_ie(jQ("#sm_olay"));	
 			if(h.config.show) h.config.show();
-			$.modal.hide(ele); //add hiding
+			$.smart_modal.hide(ele); //add hiding
 		},
 		resize_container: function(){
 			var max_width = 0, max_height=0;
-			jQ('#modal_content *').load(function(){				
-				jQ('#modal_content *').each(function(){					
+			jQ('#sm_content *').load(function(){				
+				jQ('#sm_content *').each(function(){					
 					var tw = jQ(this).outerWidth(), th = jQ(this).outerHeight();
 					if(tw > max_width) max_width = tw;
 					max_height += th;
 				});
-				if(max_width >0 && max_height>0) jQ('#modal_content').css('width', (max_width+jQ('#modal_content .modal_close:first').outerWidth())+'px').css('height', (max_height)+'px').css('margin-left', '-'+(max_width/2)+'px');
+				if(max_width >0 && max_height>0) jQ('#sm_content').css('width', (max_width+jQ('#sm_content .sm_close:first').outerWidth())+'px').css('height', (max_height)+'px').css('margin-left', '-'+(max_width/2)+'px');
 			});
 			
 		},
 		insert_overlay:function(){
-			if(!jQ('#modal_overlay').length) jQ("body").append('<div id="modal_overlay"></div>');
-      jQ("#modal_overlay").css({height:'100%',width:'100%',position:'fixed',left:0,top:0,'z-index':1000,opacity:50/100});
+			if(!jQ('#sm_olay').length) jQ("body").append('<div id="sm_olay"></div>');
+      jQ("#sm_olay").css({height:'100%',width:'100%',position:'fixed',left:0,top:0,'z-index':1000,opacity:50/100});
 		},
 		insert_content_container:function(){
-			if(!jQ('#modal_content').length) jQ("body").append('<div id="modal_content"></div>');
+			if(!jQ('#sm_content').length) jQ("body").append('<div id="sm_content"></div>');
 		},
 		get_content:function(trig){
-			c = "<div class='modal_close'><p>x</p></div>";
+			c = "<div class='sm_close'><p>x</p></div>";
 			if(trig.attr("rel")){ //if rel exists
 				div_id = jQ('#'+trig.attr('rel'));
 				div_class = jQ('.'+trig.attr('rel'));	
 				if(div_id.length){ c += div_id.html(); }
 				else if(div_class.length){ c += div_class.html();	}
 			}else if(trig.attr('href')){ //if it has a href but no rel then insert the href as image src
-				if(trig.attr('title')){ c +="<h3 class='modal_title'>"+trig.attr('title')+"</h3><img src='"+trig.attr('href')+"' alt='"+trig.attr('title')+"' />"; 	}
-				else{ c += "<img src='"+trig.attr('href')+"' alt='modal' />";	}
+				if(trig.attr('title')){ c +="<h3 class='sm_title'>"+trig.attr('title')+"</h3><img src='"+trig.attr('href')+"' alt='"+trig.attr('title')+"' />"; 	}
+				else{ c += "<img src='"+trig.attr('href')+"' alt='sm_modal' />";	}
 			}else{ c = c + trig.html(); }
 			return c;
 		},
@@ -109,6 +117,6 @@
 			}
 		}
 	};
-	var H=$.modal.hash,	jQ = jQuery;
+	var H=$.smart_modal.hash,	jQ = jQuery;
 			ie6=$.browser.msie&&($.browser.version == "6.0");
 })(jQuery);
