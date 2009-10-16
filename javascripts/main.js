5b370e1e583dbeceb32b176ba913be963463a2dc
charles marshall <charlesmarshall@charles-marshall.local>
2009-10-16 20:16:44 +0100

    updated

---
+++ javascripts/main.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+  page_init();
+});
+function page_init(){
+  cat_list_accordion();
+}
+
+function cat_list_accordion(){
+  jQuery('#category_list').accordion();
+}
+
