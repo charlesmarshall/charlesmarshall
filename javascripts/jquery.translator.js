77e31c72e2deea0f580836abbbc9ca6e1bf2b32c
charlesmarshall <cm56marshall@googlemail.com>
2010-01-24 15:45:46 +0000

    debug that

--- javascripts/jquery.translator.js
+++ javascripts/jquery.translator.js
@@ -51,8 +51,8 @@ jQuery.fn.tag = function() {
     },
     translate:function(usecount, ele){
       var trans = (jQuery(ele).length) ? jQuery(ele) : false,          
-          origin_lang = (P[usecount].origin_language.length > 1) ? jQuery(P[usecount].origin_language) : '',          
-          dest_lang = (P[usecount].result_language.length > 1) ? jQuery(P[usecount].result_language) : '',          
+          origin_lang = (jQuery(P[usecount].origin_language).length && P[usecount].origin_language.length > 1) ? jQuery(P[usecount].origin_language) : '',          
+          dest_lang = (jQuery(P[usecount].result_language).length && P[usecount].result_language.length > 1) ? jQuery(P[usecount].result_language) : '',          
           dest = (P[usecount].result.length > 1 && jQuery(P[usecount].result).length) ? jQuery(P[usecount].result) : false,
           tagname = '',
           translate = '',
