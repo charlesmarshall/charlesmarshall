746fa1ea702817cd4d413717e89f31746202a3f1
charlesmarshall <cm56marshall@googlemail.com>
2010-01-24 14:32:44 +0000

    testing

--- javascripts/jquery.translator.js
+++ javascripts/jquery.translator.js
@@ -57,13 +57,13 @@ jQuery.fn.tagName = function() {
 
       if(trans_tag == 'input' || trans_tag == 'select' || trans_tag == 'textarea') translate = trans.val();
       else translate = trans.text();
-      console.log(translate);
       
-      //    
-      //    if(origin_lang.length){        
-      //      if(origin_lang_tag == 'input' || origin_lang_tag == 'select' || origin_lang_tag == 'textarea') original_language = origin_lang.val();
-      //      else original_language = origin_lang.text();        
-      //    }else original_language = P[usecount].origin_language;
+      if(origin_lang.length){        
+        if(origin_lang_tag == 'input' || origin_lang_tag == 'select' || origin_lang_tag == 'textarea') original_language = origin_lang.val();
+        else original_language = origin_lang.text();        
+      }else original_language = P[usecount].origin_language;
+      
+      console.log(original_language);
       //    
       //    if(dest_lang.length){        
       //      if(dest_lang_tag == 'input' || dest_lang_tag == 'select' || dest_lang_tag == 'textarea') dest_language = dest_lang.val();
