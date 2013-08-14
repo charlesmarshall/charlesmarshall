18fa600419394b619e4b6307578ce1f4497183ec
Charles Marshall <charles@oneblackbear.com>
2013-08-14 22:45:50 +0100

    files for the demo

---
+++ demos/postcode-map/javascripts/javascript.js
@@ -0,0 +1,17 @@
+function initialize() {
+  var uk = new google.maps.LatLng(52.494069,-1.890442);
+  var mapOptions = {
+    zoom: 11,
+    center: uk,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var map = new google.maps.Map(document.getElementById('googlemap'), mapOptions);
+
+  var ctaLayer = new google.maps.KmlLayer({
+    url: 'http://charlesmarshall.co.uk/demos/postcode-map/kml/birmingham.kml'
+  });
+  ctaLayer.setMap(map);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
