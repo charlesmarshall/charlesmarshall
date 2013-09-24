76eafb49c0e349a611542f88abb68a521ef4b469
Charles Marshall <charles@oneblackbear.com>
2013-09-24 14:21:26 +0100

    updated submodule for the new functionality.. new example showing the control abilities .. some more copy updates to cover the functions

--- demos/timed-event-sequence/example-js/6.js
+++ demos/timed-event-sequence/example-js/6.js
@@ -1,14 +1,29 @@
 jQuery(document).ready(function(){
+  var items = jQuery("#example-6-elements span");
 
-  jQuery(".example_block").on("click", "span", function(e){
-    var span = jQuery(this),
-          parent = span.parents(".example_block"),
-          example = parent.attr("id").replace("-elements", ""),
-          inner = jQuery("#example-6-elements .output."+example);
-          pos = parent.find("span").index(span),
-          html = inner.html(),
-          extra = "<p><strong>"+example+"</strong> pos:"+pos+"</p>"
-          ;
-    inner.html(html+extra);
+  items.timedEventSequence({
+    interval:3000,
+    beforeEvent:function(items, currentitem, newitem){
+      items.filter(".active").removeClass("active");
+      return true;
+    },
+    afterEvent:function(items, before_click_item, after_click_item){
+      after_click_item.addClass("active");
+      return true;
+    }
   });
+
+  //stop
+  jQuery("#example-6-copy").on("click", "a", function(e){
+    var clicked = jQuery(this);
+    e.preventDefault();
+    if(clicked.hasClass("stop")) items.timedEventSequence("stop");
+    else if(clicked.hasClass("start")) items.timedEventSequence("start");
+    else if(clicked.hasClass("restart")) items.timedEventSequence("restart");
+    else if(clicked.hasClass("reset")) items.timedEventSequence("reset");
+    else if(clicked.hasClass("slow")) items.timedEventSequence("option", "interval", 6000);
+    else if(clicked.hasClass("fast")) items.timedEventSequence("option", "interval", 1000);
+    else if(clicked.hasClass("normal")) items.timedEventSequence("option", "interval", 3000);
+  });
+
 });
